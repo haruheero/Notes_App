cf43f2c397590748bc3884ed2034e243bc46fb5d
Pakhi Srivastava <zenith.pakhi@gmail.com>
2023-04-17 18:23:37 +0530

    Added All Notes and Upload Notes

--- Redux/Actions/AllNotesAction.js
+++ Redux/Actions/AllNotesAction.js
@@ -16,7 +16,7 @@ export const listAllNotes = () => {
             payload = [...payload, {name: itemRef._location.path_}];
         });
         dispatch({ type: "LIST_ALL_NOTES", payload: payload });
-        payload = []
+        payload = [] //Added for file reload without addition
       })
       .catch((error) => {
         console.log(error);
